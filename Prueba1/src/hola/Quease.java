d283331a85d26c59df39cb22b97619a568510c6a
nferre82 <nelsonferre82@gmail.com>
2021-05-19 20:33:21 +0200

    hola entre comillado

--- Prueba1/src/hola/Quease.java
+++ Prueba1/src/hola/Quease.java
@@ -2,8 +2,13 @@ package hola;
 
 public class Quease {
 
+	public Quease() {
+		System.out.println("lololo");
+	}
 	public static void main(String[] args) {
-		System.out.println(xD);
+		System.out.println("xD");
+		System.out.println("no soy tonto");
+		System.out.println("igual un poco si");
 
 	}
 
